f23a4a15474d71117c52bfd6f1e328056b15fabc
ganggang <just_send@163.com>
2012-05-28 12:50:35 +0800

    使用gli重构代码

--- exceptions.rb
+++
@@ -1,2 +0,0 @@
-class AccountOnlineException < Exception 
-end
--- make_request.rb
+++
@@ -1,8 +0,0 @@
-module MakeRequest 
-  def sendRequest cmd
-    post.set_form_data data
-    response = http.start {|http| http.request post}
-
-    result = WebLoginResponse.new response
-  end
-end
